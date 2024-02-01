dc9072bd65c086fa54445155de5a4a58cfeefa61
julia <sea.leaf@outlook.com>
2024-02-02 06:31:35 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 set -x
+source gamedownloaders.cfg
 docker compose -f compose.yaml build
 docker compose -f compose.yaml up -d
 docker compose -f compose.yaml logs -f
-docker attach gamedownloader
+if [ "$lgogdownloader" == 'y' ]
+then
+  docker exec -i -t gamedownloader lgogdownloader --login
+  docker exec -i -t gamedownloader lgogdownloader --list
+  docker exec -i -t gamedownloader $lgogdownloader_options
+fi
+if [ "$itchiodl" == 'y' ]
+then
+  docker exec -i -t gamedownloader python -m itchiodl.bundle_tool --api-key=$itchio_api_key
+  docker exec -i -t gamedownloader python -m itchiodl.downloader --api-key=$itchio_api_key $itchiodl_options
+fi
